d83e307952c466031c4109abe0c5e03e482bf7d7
BuildTools <unconfigured@null.spigotmc.org>
2019-10-09 18:54:56 +0100

    Environment setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-console.log("Starting")
+clearWindow();
+
+import express from "express";
+import http from "http";
+import dotenv from "dotenv";
+
+const env = dotenv.config(); // Pull env vars from ' .env ' file
+if(env.error) console.log("No environment file, using defaults");
+
+const app = express(); // Initialise express
+const server = new http.Server(app);
+
+const port = process.env.PORT || 8080;
+server.listen(port, () => {
+  console.log("Listening on:", port);
+});
+
+app.use("*", (req, res, next) => {
+  console.log(req.method, req.path, "from", req.ip);
+});
+
+
+
+
+function clearWindow() {
+  console.log("\u001b[2J\u001b[0;0H");
+}
