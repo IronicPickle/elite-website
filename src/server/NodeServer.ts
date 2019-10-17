380e8886b8d58a5a7729952941a743d2d3278a3e
Thorsten Stanitzok <thorsten.stanitzok@googlemail.com>
2019-10-17 21:37:18 +0200

    import changes and housekeeping

--- src/server/NodeServer.ts
+++ src/server/NodeServer.ts
@@ -1,14 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
-import next from "next";
 import path from "path";
 import { Express } from "express-serve-static-core";
-import Server from "next/dist/next-server/server/next-server";
 import { IncomingMessage, ServerResponse } from "http";
-import { UrlWithParsedQuery } from "url";
+import { UrlWithParsedQuery, UrlObject, Url } from "url";
+import next, { Server } from "next-server";
 
 export class NodeServer {
-
   public port: string;
   public environment: string | undefined;
   private dev: boolean;
@@ -16,15 +14,19 @@ export class NodeServer {
 
   private app: Server;
   private server: Express;
-  private handle: (req: IncomingMessage, res: ServerResponse, parsedUrl?: UrlWithParsedQuery | undefined) => Promise<void>;
+  private handle: (
+    req: IncomingMessage,
+    res: ServerResponse,
+    parsedUrl?: UrlObject | Url | undefined
+  ) => Promise<void>;
 
   constructor() {
     this.port = process.env.PORT || "8080";
     this.environment = process.env.NODE_ENV; // Node Environment
     this.dev = this.environment !== "production";
-    this.dir = (this.dev) ? "./" : "./build";
+    this.dir = this.dev ? "./" : "./build";
 
-    this.app = next({ dev: this.dev, dir: this.dir});
+    this.app = next({ dev: this.dev, dir: this.dir });
     this.server = express(); // Initialise express
     this.handle = this.app.getRequestHandler();
 
@@ -33,28 +35,29 @@ export class NodeServer {
 
     this.server.use(bodyParser.urlencoded({ extended: false }));
     this.server.use(bodyParser.json());
-
   }
 
   start() {
     return new Promise((resolve, reject) => {
       const server = this.server;
       const port = this.port;
-      this.app.prepare().then(() => {
-        server.listen(port, () => {
-          resolve(this);
-        });
+      this.app
+        .prepare()
+        .then(() => {
+          server.listen(port, () => {
+            resolve(this);
+          });
 
-        server.all("*", (req: any, res: any, next: any) => {
-          if(this.dev) console.log(req.method, req.url, "from", req.ip);
-          this.handle(req, res);
+          server.all("*", (req: any, res: any, next: any) => {
+            if (this.dev) console.log(req.method, req.url, "from", req.ip);
+            this.app.handleRequest(req, res);
+            this.handle(req, res);
+          });
+        })
+        .catch((ex: { stack: any }) => {
+          console.error(ex.stack);
+          process.exit(1);
         });
-        
-      }).catch((ex: { stack: any; }) => {
-        console.error(ex.stack);
-        process.exit(1);
-      });
     });
   }
-
 }
