352efd67ee93cd450159afe0c8eb707ff38aa945
BuildTools <unconfigured@null.spigotmc.org>
2019-10-17 16:49:06 +0100

    Reverted env changes

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,7 @@ import { NodeServer } from "./server/NodeServer";
 console.log("\n[Server] Starting...\n");
 
 const env: dotenv.DotenvConfigOutput = dotenv.config(); // Pull env vars from ' .env ' file
-if(env.error) {
-  console.log("No environment file, using defaults");
-  process.env.PORT = "8080";
-}
+if(env.error) console.log("No environment file, using defaults");
 
 const nodeServer = new NodeServer();
 nodeServer.start().then(() => {
