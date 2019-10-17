780e48f467bc28a449eac64b22e1be994415b825
BuildTools <unconfigured@null.spigotmc.org>
2019-10-17 23:33:22 +0100

    Removed Next.js and implemented client-side React

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import dotenv from "dotenv";
 import { NodeServer } from "./server/NodeServer";
 
-console.log("\n[Server] Starting...\n");
+console.log("\nStarting...\n");
 
 const env: dotenv.DotenvConfigOutput = dotenv.config(); // Pull env vars from ' .env ' file
 if(env.error) console.log("No environment file, using defaults");
 
 const nodeServer = new NodeServer();
 nodeServer.start().then(() => {
-  console.log("\n[Server] Started in:", nodeServer.environment, "mode");
-  console.log("[Server] Listening on:", nodeServer.port, "\n");
+  console.log("\nStarted in:", nodeServer.environment, "mode");
+  console.log("Listening on:", nodeServer.port, "\n");
 }).catch(console.error);
